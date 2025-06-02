64645985f88610b5bfa1c12d3adba6bc76a0604c
Subrat Hota <msubha053@gmail.com>
2025-06-02 23:55:58 +0530

    Minor Changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
-import App from "./App";
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const Game = () => {
+  return (
+    <>
+    <h1>Hii</h1>
+    <App name='Calculator'/>
+    </>
+  );
+}
+
+const App = (props) => <h1>{props.name}</h1>; 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
-    <App />
-    <App />
+    {/* <App /> */}
+    <Game/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
